15a28bde6dddf672d9c2ea5c46ebe8af01f3aaf5
Pentico <morulaneat@gmail.com>
2016-09-12 02:07:56 +0200

    Update mysqldb.class.php

--- Registry/mysqldb.class.php
+++ Registry/mysqldb.class.php
@@ -9,5 +9,60 @@
  */
 class Mysqldb
 {
+    /**
+     * Allows multiple database connections each connection
+     * is stored as an element in the array, and the active
+     * connection is maintained in a variable
+     * @var array
+     */
+    private $connections = array();
+
+    /**
+     * Tells the DB object which connection to use
+     * setActiveConnection($id) allows us to change this
+     * @var int
+     */
+    private $activeConnection = 0;
+
+    /**
+     * Queries which have been executed and the results cached for later,
+     * primarily within the template engine
+     * @var array
+     */
+    private $queryCache =array();
+
+    /**
+     * Data which has been prepared and then cached for later usage,
+     * primarily within the template engine
+     * @var array
+     */
+    private $dataCache = array();
+
+    /**
+     * Number of queries made during execution process
+     * @var int
+     */
+    private $queryCounter = 0;
+
+    /**
+     * Record of the last query
+     * @var
+     */
+    private $last;
+
+    /**
+     * Reference to the registry object
+     * @var Registry
+     */
+    private $registry;
+
+    /**
+     * Mysqldb constructor.
+     * @param Registry $registry
+     */
+    public function __construct( Registry $registry){
+        $this->registry = $registry;
+    }
+
 
 }
