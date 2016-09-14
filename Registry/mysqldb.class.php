fcbe4b74958015a21bdd10688c9428f94405e298
Pentico <morulaneat@gmail.com>
2016-09-14 20:46:30 +0200

    Completed the mysqldb.class.php ... yet to debug it

--- Registry/mysqldb.class.php
+++ Registry/mysqldb.class.php
@@ -65,4 +65,175 @@ class Mysqldb
     }
 
 
+    /**
+     * Create a new database connection
+     * @param $host
+     * @param $user
+     * @param $password
+     * @param $database
+     * @return int
+     */
+    public function newConnection($host, $user, $password, $database) {
+
+        $this->connections[] = new mysqli($host, $user, $password, $database);
+        $connection_id = count($this->connections)-1;
+        if (mysqli_connect_errno()){
+
+            trigger_error('Error connecting to host. '
+                .$this->connections[$connection_id]->error, E_USER_ERROR);
+        }
+
+        return $connection_id;
+    }
+
+    /**
+     * Change which database connection is actively used for the next operation
+     * @param int  $new
+     */
+    public function setActiveConnection( $new) {
+        $this->activeConnection = $new;
+    }
+
+
+    /**
+     * Execute a query string
+     * @param $queryStr
+     */
+    public function executeQuery( $queryStr) {
+
+        if (!$result = $this->connections[$this->activeConnection]->query($queryStr)){
+            trigger_error('Error executing query:' .$queryStr .'- '
+                .$this->connections[$this->activeConnection] ->error,E_USER_ERROR);
+        }else{
+            $this->last = $result;
+        }
+    }
+
+    /**
+     * Get the rows from the most recently executed query, excluding
+     * cached queries
+     * @return array
+     */
+    public function getRows() {
+        return $this->last->fetch_array(MYSQLI_ASSOC);
+    }
+
+    /**
+     * Delete records from the database
+     * @param $table String the table to remove rows from
+     * @param $condition String condition for which rows are to be removed
+     * @param $limit int the number of rows to be removed
+     */
+    public function deleteRecords($table, $condition, $limit) {
+
+        $limit = ( $limit =='') ?'':'LIMIT '.$limit;
+        $delete = "DELETE FROM {$table} WHERE {$condition} {$limit}";
+        $this->executeQuery($delete);
+
+    }
+
+    /**
+     * Update records in the database
+     * @param $table
+     * @param $changes
+     * @param $condition
+     * @return bool
+     */
+    public function updateRecords( $table, $changes, $condition ){
+
+        $update = "UPDATE " .$table . " SET ";
+        foreach ( $changes as $field => $value ){
+
+            $update .= "`" .$field . "`='{$value}',";
+        }
+
+         // remove our trailing
+        $update = substr($update, 0, -1);
+        if ($condition != ''){
+            $update .= "WHERE " .$condition;
+        }
+        $this->executeQuery($update);
+
+        return true;
+    }
+
+    /**
+     * Insert records into the database
+     * @param $table
+     * @param $data
+     * @return bool
+     */
+    public function insertRecords( $table, $data) {
+        // setup some variables for fields and values
+        $fields ="";
+        $values ="";
+
+        // populate them
+        foreach ($data as $f => $v) {
+
+            $fields .= "`$f`,";
+            $values .= ( is_numeric( $v ) && (intval( $v ) == $v )) ?
+                $v."," : "`$v`,";
+        }
+
+        // remove our trailing
+        $fields = substr($fields, 0, -1);
+        // remove our trailing
+        $values = substr($values, 0, -1);
+
+        $insert ="INSERT INTO $table ({$fields}) VALUES({$values})";
+        //echo $insert
+        $this->executeQuery( $insert );
+        return true;
+
+    }
+
+    /**
+     * Sanitize data
+     * @param $value
+     * @return string
+     */
+    public function sanitizeData( $value ) {
+
+        // Stripslashes
+        if (get_magic_quotes_gpc() ){
+
+            $value = stripslashes( $value );
+        }
+
+        // Quote value
+        if (version_compare(phpversion(), "4.3.0" ) == "-1"){
+
+            $value = $this->connections[$this->activeConnection]->escape_string( $value );
+        }else {
+            $value = $this->connections[$this->activeConnection]->real_escape_string( $value );
+        }
+        return $value;
+    }
+
+    public function numRows() {
+
+        return $this->last->num_rows;
+    }
+
+    /**
+     * Gets the number of affected rows from the previous query
+     * @return int the number of affected rows
+     */
+    public function affectedRows() {
+
+        return $this->last->affected_rows;
+    }
+
+
+    /**
+     * destruct
+     * close all of the database connections
+     */
+    public function __destruct() {
+
+        foreach ($this->connections as $connection) {
+            $connection->close();
+        }
+    }
 }
